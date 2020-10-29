fd4342b11fa6a3a35b9405a9678349b014e97218
Fernando Nagase <nagase.fernando@gmail.com>
2020-10-28 23:11:50 -0300

    feat: implement insertion sort algorithm

--- app/main.go
+++ app/main.go
@@ -5,5 +5,8 @@ import "fmt"
 import "github.com/nagasefernando/gosorting/sorting"
 
 func main() {
-	fmt.Println(sorting.BubbleSort([]int{3, 6, 4, 1, 9, 7, 0}))
+	slice := []int{3, 6, 4, 1, 9, 7, 0}
+
+	fmt.Println("Bubble Sort: ", sorting.BubbleSort(slice))
+	fmt.Println("Insertion Sort: ", sorting.InsertionSort(slice))
 }
---
+++ sorting/insertionSort.go
@@ -0,0 +1,18 @@
+package sorting
+
+// InsertionSort sorts an array of integers using the insertion sort algorithm
+func InsertionSort(array []int) []int {
+	sortedArray := array
+
+	for i := 1; i < len(sortedArray); i++ {
+		current := sortedArray[i]
+		j := i - 1
+		for j >= 0 && current < sortedArray[j] {
+			sortedArray[j + 1] = sortedArray[j]
+			j--
+		}
+		sortedArray[j + 1] = current
+	}
+
+	return sortedArray
+}
